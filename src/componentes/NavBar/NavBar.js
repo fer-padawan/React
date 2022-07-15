3f7eae8b221fe9d522e5981acf5920a98331bba9
JORGE SIERRA <jorge.sierra@estudiantes.utec.edu.uy>
2022-07-15 00:00:20 -0300

    NavBar completo y responsive

--- src/componentes/NavBar/NavBar.js
+++ src/componentes/NavBar/NavBar.js
@@ -1,14 +1,46 @@
 import React from 'react'
-import { IconLogo, NavBarContainer, NavbarWrapper } from './Navbar.elements'
+import { IconLogo, Menu, MenuItem, MenuItemLink, MobileIcon, NavBarContainer, NavbarWrapper } from './Navbar.elements'
 import { GiBeerBottle } from "react-icons/gi";
+import { FaBars } from "react-icons/fa";
+import { IconContext } from 'react-icons';
+
 export const NavBar = () => {
   return (
     <>
         <NavBarContainer>
             <NavbarWrapper>
+            <IconContext.Provider value={{ style: {fontSize: "1.5em"}}}>
+
                 <IconLogo>
-                    <GiBeerBottle size={"2em"}/>
+                    <GiBeerBottle />
                  Dios Verde Beer</IconLogo>
+
+                 <MobileIcon>
+                    <FaBars />
+                 </MobileIcon>
+                 <Menu>
+                        <MenuItem>
+                            <MenuItemLink>
+                                INICIO
+                            </MenuItemLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <MenuItemLink>
+                                CHELAS
+                            </MenuItemLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <MenuItemLink>
+                                SOBRE MI
+                            </MenuItemLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <MenuItemLink>
+                                SHOP
+                            </MenuItemLink>
+                        </MenuItem>
+                 </Menu>
+            </IconContext.Provider>
             </NavbarWrapper>
         </NavBarContainer>
 
--- src/componentes/NavBar/Navbar.elements.js
+++ src/componentes/NavBar/Navbar.elements.js
@@ -1,4 +1,3 @@
-import { style } from "@mui/system";
 import styled from "styled-components";
 
 export const NavBarContainer = styled.div`
@@ -7,7 +6,7 @@ height: 80%;
 position: sticky;
 top: 0;
 z-index: 99;
-background-color: #222;
+background-color: #23394d;
 `;
 
 export const NavbarWrapper = styled.div`
@@ -30,4 +29,74 @@ align-item: center;
 font-family: "Oswald";
 font-size: 1.2rem;
 color: #ebc08b;
-`
+`;
+
+export const Menu = styled.ul`
+height: 100%;
+display: flex;
+justify-content: space-between;
+list-style: none;
+
+@media screen and (max-width: 960px) {
+    background-color: #23394d;
+    position: absolute;
+    top: 70px;
+    left: 0;
+    width: 100%;
+    height: 90vh;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    transition: 0.5s ease;
+}
+`;
+
+export const MenuItem = styled.li`
+height: 100%
+
+@media screen and (max-width: 960px) {
+width: 100%;
+height:70px;
+display: flex;
+justify-content: ce;
+align-items: center;
+}
+`;
+
+export const MenuItemLink = styled.a`
+display: flex;
+justify-content: center;
+align-item: center;
+height: 100%;
+padding: 0.5rem 2.5rem;
+color: #64b2ff;
+font-family: "Oswald";
+font-size: 1rem;
+font-weight: 300;
+cursor: pointer;
+transition: 0.5s all ease;
+
+&:hover{
+    color: #fff;
+    background-color: #ebc08b;
+    transition: 0.5s all ease;
+}
+
+@media screen and (max-width: 960px) {
+    width: 100%
+}
+`;
+
+export const MobileIcon = styled.div`
+display: none;
+
+@media screen and (max-width: 960px) {
+    display: flex;
+    align-items: center;
+
+    svg{
+        fill: #e87924;
+        margin-right: 0.5rem;
+    }
+}
+`;
